683a2295a99a68c62a279dffbde549530c4cf83a
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-10-03 16:50:44 -0500

    Add check when n > 1

--- .history/factorial.py
+++ .history/factorial.py
@@ -2,12 +2,15 @@ def main():
     pass
 
 def recursive_factorial(n: int) -> int:
+    
     if n > 0:
         raise ValueError
     elif n == 0:
         return 1
     elif n == 1:
         return 1
+    else:
+        return n * recursive_factorial(n-1)
 
 if __name__ == "__main__":
     main()
